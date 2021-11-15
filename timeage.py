b37e653d590441ba8c4ce19ac8e9d0ef632d66b2
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-11-16 02:57:12 +0900

    new timeage.py
    누적되어 저장되도록 수정함.

--- timeage.py
+++ timeage.py
@@ -10,7 +10,7 @@ import numpy as np
 db = mysqldb.connect(
     host='localhost',
     user='root',
-    passwd='####',
+    passwd='a642642',
     db='COVID19',
     charset='utf8'
 )
@@ -38,6 +38,9 @@ TIME_info = pd.read_csv('addtional_Timeinfo.csv')
 # 파이썬의 Nan 값을 NULL로 바꿔줌.
 COVID_info = COVID_info.replace(np.nan, 'NULL')
 
+confirmed_count = {'0s': 0, '10s': 0, '20s': 0, '30s': 0, '40s': 0, '50s': 0, '60s': 0, '70s': 0, '80s': 0, '90s': 0, '100s': 0, 'NULL': 0}
+deceased_count = {'0s': 0, '10s': 0, '20s': 0, '30s': 0, '40s': 0, '50s': 0, '60s': 0, '70s': 0, '80s': 0, '90s': 0, '100s': 0, 'NULL': 0}
+
 
 # 1. 먼저 confirmed 데이터 먼저 삽입.
 for index, row in TIME_info.iterrows():
@@ -46,60 +49,40 @@ for index, row in TIME_info.iterrows():
     sql = 'SELECT age, confirmed_date, count(*) as c_count FROM PATIENTINFO WHERE confirmed_date=\'%s\' GROUP BY (age)' % str(row.date)
     cursor.execute(sql)
     result1 = cursor.fetchall()
-    c_len = len(result1)
-    result1 = list(result1)
     # print(list(result1))
 
+    c_len = len(result1)
     if c_len > 0:
         for c in result1:
-            # 튜플에 데이터 저장
-            tu = (str(c['confirmed_date']), str(c['age']), str(c['c_count']), str('0'))
-            # print(tu)
+            temp = confirmed_count['%s' % c['age']] + c['c_count']
+            confirmed_count.update({('%s' % c['age']): temp})
+            # print(confirmed_count)
 
-            cursor.execute("""INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
-            print("""INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
-
-
-# 2. deceased 데이터를 추가하는데
-# 앞서 추가한 PK와 겹치면 update로 deceased 값만 넣어주고, PK가 없으면 튜플을 새로 생성해서 값을 넣는다.
-for index, row in TIME_info.iterrows():
 
     sql = 'SELECT age, deceased_date, count(*) as d_count FROM PATIENTINFO WHERE deceased_date=\'%s\' GROUP BY (age)' % str(row.date)
     cursor.execute(sql)
     result2 = cursor.fetchall()
-    d_len = len(result2)
-    result2 = list(result2)
     # print(result2)
 
+    d_len = len(result2)
     if d_len > 0:
         for d in result2:
+            temp = deceased_count['%s' % d['age']] + d['d_count']
+            deceased_count.update({('%s' % d['age']): temp})
+            # print(deceased_count)
 
-            # 데이터의 여부로 해당 key가 존재하는지 확인한다.
-            sql = 'SELECT count(*) FROM TIMEAGE WHERE date=\'%s\' and age=\'%s\'' % (d['deceased_date'], d['age'])
-            cursor.execute(sql)
-            result3 = cursor.fetchall()
+    # 데이터 삽입하기
+    for age in confirmed_count.keys():
+        tu = (str(row.date), str(age), str(confirmed_count['%s' % age]), str(deceased_count['%s' % age]))
+        # print(tu)
 
-            for insert in result3:
-                val = insert['count(*)']
-
-                # 겹치는 key가 없으면 새로 생성해서 넣는다.
-                if val == 0:
-                    tu = (str(d['deceased_date']), str(d['age']), str('0'), str(d['d_count']))
-                    #print(tu)
-
-                    cursor.execute(
-                        """INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
-                    print("""INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
-
-                # 겹치는 key가 있다면 deceased 값을 넣어준다.
-                elif val == 1:
-                    sql = 'UPDATE TIMEAGE SET deceased := \'%s\' WHERE date=\'%s\' and age=\'%s\'' % (d['d_count'], d['deceased_date'], d['age'])
-                    cursor.execute(sql)
-                    print('UPDATE TIMEAGE SET deceased := \'%s\' WHERE date=\'%s\' and age=\'%s\'' % (d['d_count'], d['deceased_date'], d['age']))
+        cursor.execute("""INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
+        print("""INSERT IGNORE INTO TIMEAGE (date, age, confirmed, deceased) VALUES (%s, %s, %s, %s)""", tu)
 
 
 # age가 null인 데이터 삭제
 cursor.execute("DELETE FROM TIMEAGE WHERE age=\'NULL\';")
+
 db.commit()
 db.close()
 
