5d265f4af5aa443d4ef35c43ca0367101f92c426
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-11-19 20:43:55 +0900

    timeinfo.php

---
+++ Timeinfo.php
@@ -0,0 +1,65 @@
+// DB TEAM PROJECT #3 - 5팀(김유진, 이지원)</br>
+
+<?php
+    $dPconfig['dbhost'] = 'localhost';
+    $dPconfig['dbuser'] = 'root';
+    $dPconfig['dbpass'] = '####';
+    $dPconfig['dbname'] = 'COVID19';
+
+    $db_host = $dPconfig['dbhost'];
+    $db_user = $dPconfig['dbuser'];
+    $db_pass = $dPconfig['dbpass'];
+    $db_name = $dPconfig['dbname'];
+
+    $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    if (mysqli_connect_errno()){
+        printf("Connect failed : %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    mysqli_query($link, "set names utf8");
+?>
+
+<html>
+<style>
+    table {
+        width: 100%;
+        border: 1px solid #444444;
+        border-collapse: collapse;
+    }
+    th, td {
+        border: 1px solid #444444;
+    }
+</style>
+<body>
+    <?php
+        $sql="select count(*) from TIMEINFO;";
+        $result = mysqli_query($link, $sql);
+        $data = mysqli_fetch_assoc($result);
+        print "<p><h3>TIMEINFO table (Currently " . $data['count(*)'] . " rows in database)</h3></p>";
+    ?>
+    <table class = "table table-striped">
+        <tr>
+            <th>date</th>
+            <th>test</th>
+            <th>negative</th>
+            <th>confirmed</th>
+            <th>released</th>
+            <th>deceased</th>
+        </tr>
+
+        <?php
+            $sql = "select * from TIMEINFO ORDER BY date asc;";
+            $result = mysqli_query($link, $sql);
+            while($row = mysqli_fetch_assoc($result)){
+                print "<tr>";
+                foreach ($row as $key => $val){
+                    print "<td>" . $val . "</td>";
+                }
+                print "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
