ca0631d144ae012806b96f77d7e21afe3d53ac9e
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-11-30 03:49:08 +0900

    Update month_infection_case_age.php

--- month_infection_case_age.php
+++ month_infection_case_age.php
@@ -38,12 +38,12 @@
     
     <select name = "month">
       <option value="" selected disabled hidden>== 달을 선택하세요 ==</option>
-      <option value="01">1월</option>
-      <option value="02">2월</option>
-      <option value="03">3월</option>
-      <option value="04">4월</option>
-      <option value="05">5월</option>
-      <option value="06">6월</option>
+      <option value="01"<?php if($_POST['month']=='01') echo " selected";?>>1월</option>
+      <option value="02"<?php if($_POST['month']=='02') echo " selected";?>>2월</option>
+      <option value="03"<?php if($_POST['month']=='03') echo " selected";?>>3월</option>
+      <option value="04"<?php if($_POST['month']=='04') echo " selected";?>>4월</option>
+      <option value="05"<?php if($_POST['month']=='05') echo " selected";?>>5월</option>
+      <option value="06"<?php if($_POST['month']=='06') echo " selected";?>>6월</option>
     </select>
 
     <input type="submit" value="Submit"/>
@@ -51,11 +51,14 @@
 
 
   <?php
-    if(isset($_POST['month']))  //isset이용
+    if(isset($_POST['month'])){  //isset이용
       //echo $_POST['month'];
-      $sql = "select infection_case from patientinfo where confirmed_date like '%-{$_POST['month']}-%' group by infection_case;";
+      $month = $_POST['month'];
+      $sql = "select infection_case from patientinfo where confirmed_date like '%-{$month}-%' group by infection_case;";
       $result = mysqli_query($link, $sql);
+    }
   ?>
+
   <form>
       <select name = "infCase">
       <option value="" selected disabled hidden>== 케이스를 선택하세요 ==</option>
@@ -65,44 +68,56 @@
         <option value="<?php echo $row['infection_case']; ?>"> <?php echo $row['infection_case']; ?></option>
       <?php } ?>
       </select>
-    <input type="submit" value="Submit"/>
+    <input type="submit" value="Submit2"/>
   </form>
 
 
-    <h2>
-      <?php
+  <?php
+    //echo $month;
+
+    if(isset($_POST['infCase'])){  //isset이용
+      //echo $_POST['month'];
+      //$sql = "select infection_case from patientinfo where confirmed_date like '%-{$_POST['month']}-%' group by infection_case;";
+      //$result = mysqli_query($link, $sql);'
+      echo 'here!!!!!!!';
+    }
+  ?>
+
+    <?php
+      //echo '<h2>';
       //select 박스의 선택 내용 가져오기 하면 끝
-        if(isset($_POST['infCase']))  //isset이용
-          for ($s = 0; ;$s = $s + 1){
-            if ($caseName[$s] === $_POST['infCase']){
-              $sql = "select age, count(*) from patientinfo Where infection_case = '{$caseName[$s]}' and confirmed_date like '%-{$_POST['month']}-%' group by age;";
-              echo $sql;
-              break;
-            }
-          }
-        else{
-          $caseName[$i] = "All";  $s = $i;
-          $sql = "select * from cases order by infection_case ;";
-        }
-         
+
+      if(isset($_POST['infCase'])){  //isset이용
+        $sql = "select age, count(*) from patientinfo Where infection_case = '{$_POST['infCase']}' and confirmed_date like '%-{$_POST['month']}-%' group by age;";
+        echo $sql;
         $resultset = mysqli_query($link, $sql);
         $count = mysqli_num_rows($resultset);
-        echo 'Cases table (Currently ' . $count . ') case in database which case is '. $caseName[$s] . ' <br>';
-      ?>
-    </h2>
-    <table class="table table-striped">
-      <tr>
-          <th>Age</th>
-          <th>Count</th>
-      </tr>
-      <?php while ($row = mysqli_fetch_assoc($resultset)) {
-          print "<tr>";
-          foreach ($row as $key => $val) {
-              print "<td>" . $val . "</td>";
+        //echo "Cases table (Currently ' . $count . ') case in database which case is '. $_POST['infCase'] .' <br>";
+    
+      //echo '</h2>';
+
+        echo 'what the fuck';
+        echo '<table class="table table-striped">';
+        echo '<tr>';
+        echo '<th>Age</th>';
+        echo '<th>Count</th>';
+        echo '</tr>';
+          while ($row = mysqli_fetch_assoc($resultset)) 
+          {
+            print "<tr>";
+            foreach ($row as $key => $val) 
+            {
+                print "<td>" . $val . "</td>";
+            }
+            print "</tr>";
           }
-          print "</tr>";
+        echo '</table>';
       }
-      ?>
-    </table>
+      else {
+        echo 'what the fuck';
+        $val = isset($_POST['infCase']);
+        echo $val;
+      }
+    ?>
 </body>
 </html>
