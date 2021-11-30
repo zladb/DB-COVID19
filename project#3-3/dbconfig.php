03dedf033a3b0646f19d37a9a036f0e81ad4c09b
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-11-30 17:19:17 +0900

    Add files via upload

---
+++ project#3-3/dbconfig.php
@@ -0,0 +1,20 @@
+<?php
+    $dPconfig['dbhost'] = 'localhost';
+    $dPconfig['dbuser'] = 'root';
+    $dPconfig['dbpass'] = '####';
+    $dPconfig['dbname'] = 'COVID19';
+
+    $db_host = $dPconfig['dbhost'];
+    $db_user = $dPconfig['dbuser'];
+    $db_pass = $dPconfig['dbpass'];
+    $db_name = $dPconfig['dbname'];
+
+    $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    if (mysqli_connect_errno()){
+        printf("Connect failed : %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    mysqli_query($link, "set names utf8");
+?>
