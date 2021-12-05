a77c10348af809a823d405e5efaf773b13301319
Jiwon <70554917+E-xxi@users.noreply.github.com>
2021-12-06 00:03:01 +0900

    Update hospital_map.php

--- project#4/hospital_map.php
+++ project#4/hospital_map.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="AIzaSyAl61rJN7vtP9dzHH9-2AIvyBeG_nkvL-A">//키를 발급받아 사용하세요</script>
+<script type="text/javascript" src="AIzaSyDrleHOmCgoAxPaGl0xvPQql34Mad2FnKg">//키를 발급받아 사용하세요</script>
 <style>
 #map_ma {width:100%; height:400px; clear:both; border:solid 1px red;}
 </style>
@@ -17,13 +17,13 @@
 temp = location.href.split("?");
 data=temp[1].split("&");
       $(document).ready(function() {
-         var myLatlng = new google.maps.LatLng(35.837143,128.558612); // 위치값 위도 경도
+         var myLatlng = new google.maps.LatLng(data[1],data[0]); // 위치값 위도 경도
   // 좌표값 가져오기
 
    var Y_point         =  data[1];      // Y 좌표
    var X_point         =  data[0];      // X 좌표
    var zoomLevel      = 18;            // 지도의 확대 레벨 : 숫자가 클수록 확대정도가 큼
-   var markerTitle      = "대구광역시";      // 현재 위치 마커에 마우스를 오버을때 나타나는 정보
+   var markerTitle      = data[2];      // 현재 위치 마커에 마우스를 오버을때 나타나는 정보
    var markerMaxWidth   = 300;            // 마커를 클릭했을때 나타나는 말풍선의 최대 크기
 
 // 말풍선 내용
