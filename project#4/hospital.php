85120e7a586a8ada44c0c0b6a2a5ccd4fe12c2c9
늇뉴진 <68093782+zladb@users.noreply.github.com>
2021-12-06 02:06:23 +0900

    병원 id를 검색할 수 있도록 업데이트
    해당 병원에 입원해 있는 환자 리스트를 출력함.

--- project#4/hospital.php
+++ project#4/hospital.php
@@ -16,43 +16,73 @@
     }
 </style>
 <body>
+
+    <form method="post">
+        <br><h2>병원의 id(1~41)를 입력하세요</h2>
+        <input type="text" name="h_id">
+        <input type="submit" value="전송">
+    </form> 
+
     <?php
-        $sql="select count(*) from HOSPITAL;";
+        $sql="select count(*) from PATIENTINFO;";
         $result = mysqli_query($link, $sql);
         $data = mysqli_fetch_assoc($result);
-        print "<p><h3>HOSPITAL table (Currently " . $data['count(*)'] . " rows in database)</h3></p>";
+        print "<p><h3>PATIENTINFO + HOSPITAL_ID table (Currently " . $data['count(*)'] . " rows in database)</h3></p>";
     ?>
+
     <table class = "table table-striped">
         <tr>
-            <th>id</th>
-            <th>name</th>
+            <th>patient_id</th>
+            <th>sex</th>
+            <th>age</th>
+            <th>country</th>
             <th>province</th>
             <th>city</th>
-            <th>latitude</th>
-            <th>longitude</th>
-            <th>capacity</th>
-            <th>current</th> <!- 현재 병원 ->
+            <th>infection_case</th>
+            <th>infected_by</th>
+            <th>contact_number</th>
+            <th>symptom_onset_date</th>
+            <th>confirmed_date</th>
+            <th>released_date</th>
+            <th>deceased_date</th>
+            <th>state</th>
+            <th>hospital_id</th>
         </tr>
 
-        <form action="hosptial_map.php" method="post">
-        <?php
-            $sql = "select * from HOSPITAL ORDER BY id asc;";
+    <?php
+        if(isset($_POST['h_id']))
+        {
+            $sql = "select * from PATIENTINFO where hospital_id={$_POST['h_id']} ORDER BY patient_id asc;";
             $result = mysqli_query($link, $sql);
+        }
+        else
+        {
+            $sql = "select * from PATIENTINFO ORDER BY patient_id asc;";
+            $result = mysqli_query($link, $sql);
+        }
+    ?>
+
+      <form action="hosptial_map.php" method="post">
+        <?php
             while($row = mysqli_fetch_assoc($result)){
-                foreach ($row as $key => $val){
-                  if ($key == "longitude") // x좌표
-                    $x = $val;
-                  else if($key == "latitude") //y 좌표
-                    $y = $val;
-                  if($key == "current"){  // 테이블 생성
-                    print "<td> <a href= \"http://localhost/hospital_map?x=<%=<?php $x ?>%>&y=<%=<?php $y ?>%>&h=<%=<?php $val ?>%>\">" . $val . "</a> </td>";
-                  }else{
-                      print "<td>" . $val . "</td>";
+                foreach ($row as $key => $val)
+                {
+                    if($key == "hospital_id")
+                    {  // 테이블 생성
+                        $sql = "select latitude, longitude from HOSPITAL WHERE id = {$val}";
+                        $result2 = mysqli_query($link, $sql);
+                        $point = mysqli_fetch_assoc($result2);
+                        $x = $point["longitude"];   // 경도
+                        $y = $point["latitude"];    // 위도
+                        print "<td> <a href= \"http://localhost/hospital_map?x=<%=<?php $x ?>%>&y=<%=<?php $y ?>%>&h=<%=<?php $val ?>%>\">" . $val . "</a> </td>"; 
                     }
+                    else
+                    print "<td>" . $val . "</td>";
                 }
                 print "</tr>";
             }
         ?>
+
       </form>
     </table>
 </body>
